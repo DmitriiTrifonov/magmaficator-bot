5b20617e108c88073a12aaa62b900fdea8a44690
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2020-02-27 15:07:36 +0300

    [photo] - trying to decode a photo

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 	b.Handle(tb.OnPhoto, func(m *tb.Message) {
 		caption := m.Caption
 		log.Println(caption)
-		photo := m.Photo.FileReader
+		photo := m.Photo
 		log.Println(photo)
 		_, _ = b.Send(m.Sender, caption)
 	})
