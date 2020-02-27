0172bfeed4726238f1375f950ddf80f399dc6759
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2020-02-27 14:10:29 +0300

    [start] - added a start message

--- main.go
+++ main.go
@@ -26,8 +26,22 @@ func main() {
 		log.Fatal(err)
 	}
 
+	message := "This is the Mamgafier bot.\n" +
+		"It uses block cipher \"Mamga\" from GOST 34.12-2018.\n" +
+		"It's only ciphering green and blue channels for entertainment purposes.\n" +
+		"If you want to set a custom key please add a message to your photo" +
+		"Have fun!"
+
 	b.Handle("/start", func(m *tb.Message) {
-		_, _ = b.Send(m.Sender, "This is the Mamgafier bot.\n It uses block cipher \"Mamga\" from GOST  ")
+		_, _ = b.Send(m.Sender, message)
+	})
+
+	b.Handle("/help", func(m *tb.Message) {
+		_, _ = b.Send(m.Sender, message)
+	})
+
+	b.Handle("/about", func(m *tb.Message) {
+		_, _ = b.Send(m.Sender, message)
 	})
 
 	b.Handle(tb.OnPhoto, func(m *tb.Message) {
