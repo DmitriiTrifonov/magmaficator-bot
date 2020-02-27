6e934e99e4fb42b16bad2d9785084d1ce9623bec
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2020-02-27 12:42:42 +0300

    [helloworld] -- added hello world

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	tb "gopkg.in/tucnak/telebot.v2"
+	"log"
+	"os"
+)
 
 func main() {
-    fmt.Println("Henlo")
+	port := os.Getenv("PORT")
+	publicURL := os.Getenv("PUBLIC_URL")
+	token := os.Getenv("TOKEN")
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Handle("/start", func(m *tb.Message) {
+		_, _ = b.Send(m.Sender, "Henlo!")
+	})
+
+	b.Handle("/hello", func(m *tb.Message) {
+		_, _ = b.Send(m.Sender, "Henlo!")
+	})
+
 }
